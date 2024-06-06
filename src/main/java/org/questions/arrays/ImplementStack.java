ec42a70d01d9ef97829a9f22b5761acf5887bc66
souvik943 <souvikkarmakar2k19@gmail.com>
2024-06-06 21:12:03 +0530

    DS - Questions

---
+++ src/main/java/org/questions/arrays/ImplementStack.java
@@ -0,0 +1,33 @@
+package org.questions.arrays;
+
+public class ImplementStack {
+
+    int arr[] = new int[5];
+    int top = -1;
+    void push(int data) {
+        System.out.println("Pushed : " + data);
+        top++;
+        arr[top] = data;
+    }
+
+    void pop() {
+        int popped = arr[top];
+        System.out.println("Popped : " + arr[top]);
+        top--;
+    }
+
+    void top() {
+        System.out.println("Top : " + arr[top]);
+    }
+
+    public static void main(String[] args) {
+        ImplementStack stack = new ImplementStack();
+        stack.push(3);
+        stack.push(4);
+        stack.push(23);
+        stack.push(0);
+        stack.top();
+        stack.pop();
+        stack.top();
+    }
+}
