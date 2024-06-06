ce1fd92dc55b2293cb6f70c1f9a2e38c6912d223
souvik943 <souvikkarmakar2k19@gmail.com>
2024-06-06 22:01:57 +0530

    Array : Linear-Search

---
+++ src/main/java/org/questions/arrays/search/LinearSearch.java
@@ -0,0 +1,24 @@
+package org.questions.arrays.search;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {2,23,45,56,67,78};
+        System.out.println(find(arr,56));
+        System.out.println(find(arr,65));
+        System.out.println(find(arr,100));
+    }
+
+    static boolean find(int[] arr, int target) {
+        boolean founded = false;
+        for(int i : arr) {
+            if (i == target) {
+                founded = true;
+                break;
+            }
+            if(i > target) {
+                break;
+            }
+        }
+        return founded;
+    }
+}
