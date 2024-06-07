f57a53970b4e36fb46f03e67b33692dff0d46bf6
souvik943 <souvikkarmakar2k19@gmail.com>
2024-06-07 13:49:45 +0530

    Array : Second Largest Element

---
+++ src/main/java/org/questions/arrays/SecondLargest.java
@@ -0,0 +1,21 @@
+package org.questions.arrays;
+
+public class SecondLargest {
+
+    public static void main(String[] args) {
+        int[] arr = {61 ,30 ,28 ,47 ,42 ,25 ,41 ,56 ,27 ,45 ,44 ,34 ,46 ,35 ,58 ,36 ,60 ,29 ,53 ,55 ,32 ,31 ,33 ,59 ,50 ,51 ,52 ,37 ,39 ,38 ,43 ,49 ,54 ,57 ,40 ,26 ,48};
+        System.out.println(secondLargest(arr));
+    }
+
+    static int secondLargest(int[] arr) {
+        int largest = arr[0];
+        int secondLargest = -1;
+        for(int i : arr) {
+            if(i > largest) largest = i;
+        }
+        for(int i : arr) {
+            if(i > secondLargest && i != largest) secondLargest = i;
+        }
+        return secondLargest;
+    }
+}
