ae775e57f76222c3211f1d44e4bce2c8c06df522
souvik943 <souvikkarmakar2k19@gmail.com>
2024-06-07 13:49:23 +0530

    Array : Product Except Self

---
+++ src/main/java/org/questions/arrays/ProductExceptSelf.java
@@ -0,0 +1,33 @@
+package org.questions.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductExceptSelf {
+    public static void main(String[] args) {
+        int[] arr = {2,1,3,4};
+        productExceptSelf(arr);
+    }
+
+    static void productExceptSelf(int[] arr) {
+        int[] left = new int[arr.length];
+        int[] right = new int[arr.length];
+
+        left[0] = 1;
+        right[arr.length-1] = 1;
+        for(int i = 1; i < arr.length; i++) {
+            left[i] = left[i-1] * arr[i-1];
+        }
+
+        for(int i = arr.length-2; i>-1; i--) {
+            right[i] = right[i+1] * arr[i+1];
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = left[i] * right[i];
+        }
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
