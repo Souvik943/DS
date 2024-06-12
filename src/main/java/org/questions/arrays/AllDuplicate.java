83e8acfed25c174c4affede336ab1e0fc1805d10
souvik943 <souvikkarmakar2k19@gmail.com>
2024-06-12 21:41:33 +0530

    Array Questions

---
+++ src/main/java/org/questions/arrays/AllDuplicate.java
@@ -0,0 +1,41 @@
+package org.questions.arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+public class AllDuplicate {
+
+    public static void main(String[] args) {
+        int[] arr = {1};
+        System.out.println(findDuplicates(arr));
+    }
+
+//--------------------- Brute Force --------------------------
+//    static List<Integer> findDuplicates(int[] arr) {
+//        List<Integer> ans = new ArrayList<>();
+//        HashSet<Integer> hashSet = new HashSet<>();
+//        for(int i:arr) {
+//            if(hashSet.contains(i)) {
+//                ans.add(i);
+//            }
+//            hashSet.add(i);
+//        }
+//        Collections.sort(ans);
+//        return ans;
+//    }
+
+    static List<Integer> findDuplicates(int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i=0;i<arr.length;i++) {
+            int goToIndex = Math.abs(arr[i]) - 1;
+            if(arr[goToIndex] < 0) {
+                ans.add(goToIndex+1);
+            }
+            arr[goToIndex] = arr[goToIndex]*-1;
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+}
--- src/main/java/org/questions/arrays/CountPairsWithGivenSum.java
+++ src/main/java/org/questions/arrays/CountPairsWithGivenSum.java
@@ -9,14 +9,25 @@ public class CountPairsWithGivenSum {
         System.out.println(getPairsCount(arr,2));
     }
 //------------------------ Brute-Force ----------------------------------
+//    static int getPairsCount(int[] arr, int target) {
+//        int count = 0;
+//        for(int i=0; i<arr.length; i++) {
+//            for(int j=i+1; j<arr.length; j++) {
+//                if(arr[i] + arr[j] == target) count++;
+//            }
+//        }
+//        return count;
+//    }
+
     static int getPairsCount(int[] arr, int target) {
         int count = 0;
-        for(int i=0; i<arr.length; i++) {
-            for(int j=i+1; j<arr.length; j++) {
-                if(arr[i] + arr[j] == target) count++;
-            }
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+            if(hashMap.containsKey(complement)) count++;
+            hashMap.put(complement,i);
         }
-        return count;
+        return count*2;
     }
 
 }
